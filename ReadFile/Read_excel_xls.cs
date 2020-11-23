6d055d95414c4755d0977302ea1bc4fe163cbd5b
Денис Голубев (Работа) <amigo36@yandex.ru>
2020-11-23 16:45:49 +0300

    Open file dialog, xls

---
+++ ReadFile/Read_excel_xls.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+
+namespace SuperJet_2._1.ReadFile
+{
+    class Read_excel_xls
+    {
+        Application xls_file = new Application();
+        Workbook xls_workbook = new Workbook();
+    }
+}
