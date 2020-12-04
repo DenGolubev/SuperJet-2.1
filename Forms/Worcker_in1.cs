c9918d0e3d90d4a7712dd97cca14e0be895f5714
Denis <den@dgolubev.ru>
2020-12-04 14:35:07 +0300

    Разделил class Worcker_in на два partial class Worcker_in

---
+++ Forms/Worcker_in1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperJet_2._1.Forms
+{
+    class Worcker_in1
+    {
+    }
+}
